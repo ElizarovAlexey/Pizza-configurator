e915ad3c61ffa49af30e64d0064bb987f15d348e
Alexey Elizarov <elizarov.alex2020@gmail.com>
2021-03-17 15:03:27 +0200

    Added blocks with desserts and drinks, implemented js-smooth-scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-// Change styles on the fixed header
-
+// Fixed header
 const fixedHeader = () => {
     const header = document.getElementById('header');
 
@@ -14,8 +13,8 @@ const fixedHeader = () => {
 
 fixedHeader();
 
-// Burger menu
 
+// Burger menu
 const burgerHandler = () => {
     const headerBurger = document.getElementById('header__burger');
     const navMenu = document.getElementById('nav');
@@ -37,7 +36,6 @@ burgerHandler();
 
 
 // Slider
-
 const slider = () => {
     const dots = document.querySelectorAll('.slider__dots-item');
     const dotsAria = document.getElementById('slider__dots');
@@ -83,3 +81,45 @@ const slider = () => {
 }
 
 slider();
+
+
+// Smooth scroll
+const smoothScroll = () => {
+    const smoothScroll = function (targetEl, duration) {
+        const headerHeight = document.getElementById('header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+
+        const ease = function (t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+
+        const animation = function (currentTime) {
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0, run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}
+
+smoothScroll();
